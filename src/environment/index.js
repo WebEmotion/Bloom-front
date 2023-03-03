5eba7a11a975a637c7ee4c84d736b9ac0cba3cfd
WebEmotion <119962771+WebEmotion@users.noreply.github.com>
2023-03-02 22:42:45 -0600

    Update index.js

--- src/environment/index.js
+++ src/environment/index.js
@@ -1,6 +1,6 @@
 export const COLORS = {
     background: '#12161F',
-    primary: '#d78676',
+    primary: '#3eb978',
     money: '#DE893E',
     gradient: 'linear-gradient(180deg, rgba(69,182,167,1) 0%, rgba(120,205,176,1) 100%)',
     error: '#ED4337',
