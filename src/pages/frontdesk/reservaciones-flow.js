459014628399c977d63622fa3b70dd3cc4dd8e2a
WebEmotion <119962771+WebEmotion@users.noreply.github.com>
2023-03-10 23:12:55 +0000

    modified:   src/pages/frontdesk/reservaciones-flow.js

--- src/pages/frontdesk/reservaciones-flow.js
+++ src/pages/frontdesk/reservaciones-flow.js
@@ -5,10 +5,10 @@ import { inject, observer } from "mobx-react"
 import { Planet } from "react-planet"
 import { navigate } from "gatsby"
 import { Button } from "primereact/button"
+import bici from "../../assets/images/icons8-camino-de-bicis-100.png"
 
 import Logo from "../../assets/images/cicle_white.png"
 import Tree from "../../assets/images/tree.png"
-import bici from "../../assets/images/icons8-camino-de-bicis-100.png"
 import * as SchedulesAPI from '../../api/v0/schedules'
 
 const UserCircle = ({ image, name, lastname, seat }) => {
@@ -32,7 +32,7 @@ const UserCircle = ({ image, name, lastname, seat }) => {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <p style={{margin: 0}}>{seat}</p>
-            <img src={bici} style={{ backgroundColor: '#3eb978', width: 50, height: 50, borderRadius: '100%', padding: 5 }} />
+            <img src={{bici}} style={{ backgroundColor: '#3eb978', width: 50, height: 50, borderRadius: '100%', padding: 5 }} />
             <p style={{ margin: 0 }}> </p>
         </div>
     )
@@ -73,7 +73,7 @@ const Reservaciones = inject("RootStore")(
                 }, 60000);
                 return () => clearInterval(interval)
             } else {
-              //  navigate("/frontdesk/lugares")
+               // navigate("/frontdesk/lugares")
             }
         }, [])
 
@@ -94,13 +94,12 @@ const Reservaciones = inject("RootStore")(
             <div className="mapa-cycling-list">
                 <div style={{position: 'fixed', top: 20, left: 20}}>
                     <Button icon="pi pi-times" className="p-button-rounded p-button-pink" onClick={() => {
-                        navigate('/frontdesk/lugares-flow')
+                        navigate('/frontdesk/lugares')
                     }} />
                 </div>
                 {/* marginTop normal (stage down), marginBottom for 180° (stage up) */}
                 <div style={{ marginBottom: 500 }}>
-
-                    <div
+                    <Planet
                         centerContent={
                             <div
                                 style={{
@@ -112,6 +111,10 @@ const Reservaciones = inject("RootStore")(
                             />
                         }
                         open
+                        autoClose={false}
+                        orbitRadius={480}
+                        //86 normal(stage down), 266 for 180° (stage up)
+                        rotation={266}
                         orbitStyle={() => ({
                             border: "none",
                             zIndex: 0,
@@ -119,14 +122,80 @@ const Reservaciones = inject("RootStore")(
                             borderRadius: "100%",
                         })}
                     >
-                        <h1 className="fila"></h1>
+                        <h1 className="fila">C</h1>
+
+                        {/* divs for a tree that occupies 2 places */}
+                        <div></div>
+                        <div>
+                            <div
+                                style={{
+                                width: 130,
+                                height: 130,
+                                borderRadius: '100%',
+                                backgroundColor: "#7aa578",
+                                position: "absolute",
+                                transform: "translateX(-80px)",
+                                display: "flex",
+                                alignItems: "center",
+                                color: "#fff",
+                                textAlign: "center",
+                                justifyContent: "center",
+                                marginTop: "-80px",
+                                marginLeft: 20
+                                }}
+                            >
+                                <div className="p-grid p-align-center p-justify-center">
+                                    <div className="p-col-12">
+                                        <img
+                                            style={{
+                                                maxWidth: "100%",
+                                                marginBottom: "0.5rem",
+                                                marginTop: -30,
+                                                marginRight: 30,
+                                                display: 'none'
+                                            }}
+                                            src={Tree}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                style={{
+                                width: 130,
+                                height: 130,
+                                borderRadius: '100%',
+                                position: "absolute",
+                                transform: "translateX(-80px)",
+                                display: "flex",
+                                alignItems: "center",
+                                color: "#fff",
+                                textAlign: "center",
+                                justifyContent: "center",
+                                marginLeft: 20
+                                }}
+                            >
+                                <div className="p-grid p-align-center p-justify-center">
+                                    <div className="p-col-12">
+                                        <p
+                                        style={{
+                                            marginBottom: 0,
+                                            color: 'black'
+                                        }}
+                                        >
+                                        The Soul
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
                         {
-                            [9, 10, 11, 12, 13, 14, 15,].map(n => {
-                                const user = getUser(n + 8, n)
-                                return <UserCircle seat={`${n}`} image={user ? user.pictureUrl : ''} name={user ? user.name : ''} />
+                            [1, 2, 3, 4, 5, 6, 7, 8, 9].map(n => {
+                                const user = getUser(n, n, "C")
+                                return <UserCircle seat={`C${n}`} image={user ? user.pictureUrl : ''} name={user ? user.name : ''} />
                             })
                         }
-                        <h1 className="fila"></h1>
+                        <h1 className="fila">C</h1>
                         {/* Number of divs is same userCircle number + 1 */}
                         <div></div>
                         <div></div>
@@ -137,8 +206,129 @@ const Reservaciones = inject("RootStore")(
                         <div></div>
                         <div></div>
                         <div></div>
-                    </div>
-                    <div
+                        <div></div>
+
+                        {/* divs for a tree that occupies 2 places */}
+                        <div></div>
+                        <div></div>
+                    </Planet>
+                    <Planet
+                        centerContent={
+                            <div
+                                style={{
+                                    height: 50,
+                                    width: 100,
+                                    backgroundColor: "#3eb978",
+                                    display: "none",
+                                }}
+                            />
+                        }
+                        open
+                        autoClose={false}
+                        orbitRadius={370}
+                        //86 normal (stage down), 265 for 180° (stage up)
+                        rotation={265}
+                        orbitStyle={() => ({
+                            border: "none",
+                            zIndex: 0,
+                            position: "absolute",
+                            borderRadius: "100%",
+                        })}
+                    >
+                        <h1 className="fila">B</h1>
+                        {
+                            [1, 2, 3, 4, 5, 6, 7, 8, 9].map(n => {
+                                const user = getUser(n + 9, n, "B")
+                                return <UserCircle seat={`B${n}`} image={user ? user.pictureUrl : ''} name={user ? user.name : ''} />
+                            })
+                        }
+                        <h1 className="fila">B</h1>
+                        {/* Number of divs is same userCircle number + 1 */}
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </Planet>
+                    <Planet
+                        centerContent={
+                            <div>
+                                <div
+                                    style={{
+                                    width: 130,
+                                    height: 130,
+                                    borderRadius: '100%',
+                                    backgroundColor: "#3eb978",
+                                    position: "absolute",
+                                    transform: "translateX(-80px)",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    color: "#fff",
+                                    textAlign: "center",
+                                    justifyContent: "center",
+                                    marginTop: "-80px",
+                                    marginLeft: 20
+                                    }}
+                                >
+                                    <div className="p-grid p-align-center p-justify-center">
+                                    <div className="p-col-12">
+                                        <p
+                                        style={{
+                                            marginBottom: 0,
+                                            color: 'transparent'
+                                        }}
+                                        >
+                                        Instructor
+                                        </p>
+                                    </div>
+                                    <div className="p-col-8">
+                                        <img
+                                        style={{
+                                            maxWidth: "100%",
+                                            marginBottom: "0.5rem",
+                                            marginTop: -30,
+                                            marginRight: 30
+                                        }}
+                                        src={Logo}
+                                        />
+                                    </div>
+                                    </div>
+                                </div>
+                                <div
+                                    style={{
+                                    width: 130,
+                                    height: 130,
+                                    borderRadius: '100%',
+                                    position: "absolute",
+                                    transform: "translateX(-80px)",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    color: "#fff",
+                                    textAlign: "center",
+                                    justifyContent: "center",
+                                    marginLeft: 20
+                                    }}
+                                >
+                                    <div className="p-grid p-align-center p-justify-center">
+                                        <div className="p-col-12">
+                                            <p
+                                            style={{
+                                                marginBottom: 0,
+                                                color: 'black'
+                                            }}
+                                            >
+                                            Coach
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        }
                         open
                         autoClose={false}
                         orbitRadius={250}
@@ -168,7 +358,7 @@ const Reservaciones = inject("RootStore")(
                         <div></div>
                         <div></div>
                         <div></div>
-                    </div>
+                    </Planet>
                 </div>
             </div>
         )
