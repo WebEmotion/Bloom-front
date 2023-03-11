71201da34014f8f442985c07cf46c6e31fc792f4
WebEmotion <119962771+WebEmotion@users.noreply.github.com>
2023-03-11 19:01:51 +0000

    modified:   src/pages/mis-clases-flow.js

--- src/pages/mis-clases-flow.js
+++ src/pages/mis-clases-flow.js
@@ -12,7 +12,7 @@ import Loader from "react-loader-spinner"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Item from "../components/item-profile"
-import MisClases from "../assets/images/flow-logo.png"
+import MisClases from "../assets/images/cicle_white.jpg"
 
 import * as LocationsApi from '../api/v0/locations'
 import * as MeApi from '../api/v0/me'
