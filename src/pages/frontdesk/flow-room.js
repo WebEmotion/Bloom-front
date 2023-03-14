1d11ac0987259fe10c9031eb5e0226aa981b7168
WebEmotion <119962771+WebEmotion@users.noreply.github.com>
2023-03-14 03:46:48 +0000

    flow-image.png

--- src/pages/frontdesk/flow-room.js
+++ src/pages/frontdesk/flow-room.js
@@ -19,7 +19,8 @@ import Item from "../../components/item-profile"
 
 import * as moment from "moment"
 
-import Logo from "../assets/images/flow-image.png"
+import Logo from "../assets/images/cicle_white.png"
+//import Logo from "../assets/images/flow-image.png"
 import Tree from "../../assets/images/tree.png"
 import * as SchedulesAPI from "../../api/v0/schedules"
 import * as ClientsAPI from "../../api/v0/clients"
