c6d56bf69513287084cdc060c1d53e89b0336112
WebEmotion <119962771+WebEmotion@users.noreply.github.com>
2023-03-14 05:34:53 +0000

    modified:   src/environment/index.js

--- src/environment/index.js
+++ src/environment/index.js
@@ -20,7 +20,7 @@ export const URLS = {
 }
 
 export const API = {
-    //BASE_URL: 'https://bloom-api.com',            //Produccion .
+    //BASE_URL: 'https://bloom-api.com',            //Produccion
    BASE_URL: 'http://localhost:5200/',          //localhost
     AUTH: '/auth/'
 }
