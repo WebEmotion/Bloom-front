1814a38880b534618778e1d8fcadea0b6a3d7775
WebEmotion <119962771+WebEmotion@users.noreply.github.com>
2023-03-14 03:47:09 +0000

    modified:   src/pages/mis-clases-flow.js

--- src/pages/mis-clases-flow.js
+++ src/pages/mis-clases-flow.js
@@ -14,7 +14,7 @@ import SEO from "../components/seo"
 import Item from "../components/item-profile"
 import MisClases from "../assets/images/cicle_white.png"
 //import MisClases from "../assets/images/flow-image.png"
-
+//
 import * as LocationsApi from '../api/v0/locations'
 import * as MeApi from '../api/v0/me'
 import * as Schedules from '../api/v0/schedules'
