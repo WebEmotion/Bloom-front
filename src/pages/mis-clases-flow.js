565cf3abbff1792ae8edc174325445a449d9383d
WebEmotion <119962771+WebEmotion@users.noreply.github.com>
2023-03-14 06:38:14 +0000

    modified:   src/pages/flow-room.js 	modified:   src/pages/mis-clases-flow.js

--- src/pages/mis-clases-flow.js
+++ src/pages/mis-clases-flow.js
@@ -12,7 +12,7 @@ import Loader from "react-loader-spinner"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Item from "../components/item-profile"
-import MisClases from "../assets/images/rode-1.png"
+import MisClases from "../assets/images/flow-image.png"
 
 import * as LocationsApi from '../api/v0/locations'
 import * as MeApi from '../api/v0/me'
